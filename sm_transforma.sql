021f08ae987961360fa35f20a96febb812573cd1
ldvalle <34348282+ldvalle@users.noreply.github.com>
2020-10-30 11:51:29 -0300

    Add files via upload

---
+++ sm_transforma.sql
@@ -0,0 +1,9 @@
+create table sm_transforma (                                                                                                                            
+clave                char(10) not null,
+cod_mac_numerico     integer,
+cod_mac_alfa         char(10),
+descripcion          char(50),
+cod_smile            char(20));
+
+create index inx01sm_transforma on sm_transforma(clave, cod_mac_numerico, cod_mac_alfa);
+
