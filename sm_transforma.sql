ec5f0ce10dbf02b8d73b513ccaffd3c59afb3b5d
ldvalle <34348282+ldvalle@users.noreply.github.com>
2021-02-09 15:45:32 -0300

    Add files via upload

--- sm_transforma.sql
+++ sm_transforma.sql
@@ -6,4 +6,30 @@ descripcion          char(50),
 cod_smile            char(20));
 
 create index inx01sm_transforma on sm_transforma(clave, cod_mac_numerico, cod_mac_alfa);
+    
+GRANT select ON sm_transforma  TO
+supercal, superfat, supersre, superpjp, supersbl,
+supercri, "UCENTRO", batchsyn,pjp, ftejera, sbl,
+gtricoci, sreyes, ssalve, pablop, aarrien, vdiaz,
+ldvalle, vaz, corbacho, pmf, ctousu;
+
+GRANT insert ON sm_transforma  TO
+supercal, superfat, supersre, superpjp, supersbl,
+supercri, "UCENTRO", batchsyn,pjp, ftejera, sbl,
+gtricoci, sreyes, ssalve, pablop, aarrien, vdiaz,
+ldvalle, vaz, corbacho, pmf, ctousu;
+
+GRANT delete ON sm_transforma  TO
+supercal, superfat, supersre, superpjp, supersbl,
+supercri, "UCENTRO", batchsyn,pjp, ftejera, sbl,
+gtricoci, sreyes, ssalve, pablop, aarrien, vdiaz,
+ldvalle, vaz, corbacho, pmf, ctousu;
+
+GRANT update ON sm_transforma  TO
+supercal, superfat, supersre, superpjp, supersbl,
+supercri, "UCENTRO", batchsyn,pjp, ftejera, sbl,
+gtricoci, sreyes, ssalve, pablop, aarrien, vdiaz,
+ldvalle, vaz, corbacho, pmf, ctousu;
+
+
 
